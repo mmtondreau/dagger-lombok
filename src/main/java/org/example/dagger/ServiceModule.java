9370e5bd32a812922a8bfc4a611c232eed196e65
Matthew Tondreau <mmtondreau@gmail.com>
2021-08-11 22:47:11 -0700

    Add injection for region

--- src/main/java/org/example/dagger/ServiceModule.java
+++ src/main/java/org/example/dagger/ServiceModule.java
@@ -5,14 +5,20 @@ import dagger.Provides;
 import org.example.DBClient;
 import org.example.Service;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
 
 @Module
 public class ServiceModule {
 
+  @Named(value="region")
+  String getRegion() {
+    return System.getProperty("region", "us-west-1");
+  }
+
   @Provides
   @Singleton
-  DBClient getDBClient() {
-    return new DBClient("us-west-1");
+  DBClient getDBClient(@Named(value = "region") String region) {
+    return new DBClient(region);
   }
 }
